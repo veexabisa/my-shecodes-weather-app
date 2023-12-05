ba25eda71da6bbf6b8791e78d196e255df3a8edd
xabisavee <151728644+veexabisa@users.noreply.github.com>
2023-12-05 15:32:52 -0800

    Added search functionality using the API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temp");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = `144016bb2ce3odff4b8e4583ca9a5c1t`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metirc`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function searchButton(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchButton);
+
+searchCity("Port Elizabeth");
