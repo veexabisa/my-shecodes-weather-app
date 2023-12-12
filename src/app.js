e4c038aed73085bda0d63a8f291158527b1cc60b
xabisavee <151728644+veexabisa@users.noreply.github.com>
2023-12-12 15:18:52 -0800

    changed and updated on index.html,styles.css and app.js

--- src/app.js
+++ src/app.js
@@ -6,18 +6,17 @@ function updateWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
-  let date = new Date(response.data.time + 1000);
-  let iconElement = document.querySelector("#emoji-icon");
+  let date = new Date(response.data.time * 1000);
+  let emojiIconElement = document.querySelector("#emoji-icon");
 
-  icon.innerHTML = `<img src="$(response.data.condition.icon.url)" class="weather-emoji-icon" />`;
-
+  icon.innerHTML = `<img src=${response.data.condition.icon_url} class="weather-emoji-icon" />`;
   console.log(response.data.condition.description);
 
   cityElement.innerHTML = response.data.city;
-  timeElement.innerHTML = formatDate(date);
-  descriptionElement.innerHTML = response.data.condition.description;
+  timeElement.innerHTML = `${date.getDay()} ${date.getHours()}:${date.getMinutes()}`;
+  descriptionElement.innerHTML = `${response.data.condition.description}`;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  windSpeedElement = `${response.data.wind.speed}km/h`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
@@ -35,7 +34,6 @@ function formatDate(date) {
   ];
 
   let day = days[date.getDay()];
-
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
@@ -44,18 +42,18 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = `144016bb2ce3odff4b8e4583ca9a5c1t`;
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metirc`;
+  let apiKey = "144016bb2ce3odff4b8e4583ca9a5c1t";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(updateWeather);
 }
 
 function searchButton(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input");
+  let searchInput = document.querySelector(".search-form-input");
   searchCity(searchInput.value);
 }
 
-let searchFormElement = document.querySelector("#search-form");
+let searchFormElement = document.querySelector(".search-form");
 searchFormElement.addEventListener("submit", searchButton);
 
 searchCity("Port Elizabeth");
