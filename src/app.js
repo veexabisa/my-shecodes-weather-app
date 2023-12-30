574f385a177207636c09610e8c5540e832101beb
xabisavee <151728644+veexabisa@users.noreply.github.com>
2023-12-30 13:49:40 -0800

    Added a js forcast

--- src/app.js
+++ src/app.js
@@ -53,7 +53,31 @@ function searchButton(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      ` <div class="weather-forecast-date">${day}</div>
+                    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" class="temperature-emoji" alt="" width="42"/>
+                    <div class="weather-forecast-temperature">
+                        <span class="weather-forecast-temperature-max">
+                            18°
+                        </span>
+                        <span class="weather-forecast-temperature-min">
+                            12°
+                        </span>
+                    </div>`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector(".search-form");
 searchFormElement.addEventListener("submit", searchButton);
 
 searchCity("Port Elizabeth");
+displayForecast();
